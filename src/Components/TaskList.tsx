a500f92e92e2ff534bffeab8fe51e73831c72ab7
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-04-26 18:07:19 -0400

    update styling to task list

--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -10,11 +10,20 @@ export default function TaskList() {
 						<DeleteButton />
 					</div>
 				</li>
-				<li className='grid gap-x-2 grid-cols-2 items-center border-b-2 border-emerald-600 mb-4'>
-					<p>Another task</p>
-					<div className='w-fit justify-self-end mb-2'>
-						<DeleteButton />
-					</div>
+				<li className='flex items-center border-b-2 border-emerald-600 mb-4'>
+					<label className='flex'>
+						<p>Another task</p>
+						<input
+							type='checkbox'
+							name='taskCompletedBox'
+							id='taskCompletedBox'
+						/>
+					</label>
+					<input
+						type='submit'
+						value='Delete'
+						className=' flex w-fit justify-self-end mb-2 rounded-full bg-red-800 font-bold text-md px-4 lg:px-8'
+					/>
 				</li>
 			</ul>
 		</div>
