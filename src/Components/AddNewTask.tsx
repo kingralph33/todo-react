f48606e0bdb39336e6434ae697e55172bde023b5
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-04-26 17:52:53 -0400

    update styling of container and addNewTask form

--- src/Components/AddNewTask.tsx
+++ src/Components/AddNewTask.tsx
@@ -2,24 +2,26 @@ import AddButton from './Buttons/AddButton';
 
 export default function AddNewTask() {
 	return (
-		<div className='flex mb-6 px-8 items-center'>
-			<div className='w-11/12'>
-				<label className='flex'>
-					<span className='flex-none w-36 font-bold text-xl lg:text-2xl mr-2 text-emerald-400'>
+		<div className='mb-6 px-8'>
+			<form className='flex w-full items-center'>
+				<label htmlFor='taskInput' className='flex'>
+					<span className='flex-none w-32 font-bold text-xl lg:text-2xl text-emerald-400'>
 						Enter Task:
 					</span>
-					<input
-						type='text'
-						id='taskInput'
-						name='taskInput'
-						minLength={1}
-						className='flex-initial w-full h-12 p-2'
-					/>
 				</label>
-			</div>
-			<div className='w-1/12'>
-				<AddButton />
-			</div>
+				<input
+					type='text'
+					id='taskInput'
+					name='taskInput'
+					minLength={1}
+					className='flex-initial w-full h-12 mx-2 p-2'
+				/>
+				<input
+					type='submit'
+					value='ADD'
+					className='flex rounded-full bg-emerald-400 font-bold text-lg px-4 lg:px-8'
+				/>
+			</form>
 		</div>
 	);
 }
--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -6,13 +6,13 @@ export default function TaskList() {
 			<ul>
 				<li className='grid gap-x-2 grid-cols-2 items-center border-b-2 border-emerald-600 mb-4'>
 					<p>This is a task</p>
-					<div className='w-fit justify-self-end'>
+					<div className='w-fit justify-self-end mb-2'>
 						<DeleteButton />
 					</div>
 				</li>
 				<li className='grid gap-x-2 grid-cols-2 items-center border-b-2 border-emerald-600 mb-4'>
 					<p>Another task</p>
-					<div className='w-fit justify-self-end'>
+					<div className='w-fit justify-self-end mb-2'>
 						<DeleteButton />
 					</div>
 				</li>
