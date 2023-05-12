4e8d2ef13a3229e4ffd85a572b9fde9f5143c340
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-05-12 13:34:06 -0400

    saving work

--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -1,5 +1,5 @@
 export default function TaskList() { return ( <div className='border-4
-border-emerald-600 text-white p-8'>
+border-emerald-600 text-emerald-400 font-bold text-lg p-8'>
 			<ul>
 				<li className='flex items-center border-b-2 border-emerald-600 mb-4'>
 					<label className='flex w-full mb-2'>
@@ -8,12 +8,12 @@ border-emerald-600 text-white p-8'>
 							type='checkbox'
 							name='taskCompletedBox'
 							id='taskCompletedBox'
-                            className="flex w-5 mr-4 indeterminate:bg-gray-300 checked:accent-emerald-600"
+                            className="flex w-5 mr-4 checked:accent-emerald-400"
 						/>
                        <input
 						type='submit'
 						value='Delete'
-						className=' flex w-fit justify-self-end rounded-full bg-red-800 font-bold text-md px-4 lg:px-8'
+						className=' flex w-fit justify-self-end rounded-full bg-red-800 font-bold text-white text-md px-4 lg:px-8'
 					   />
 					</label>
 					
