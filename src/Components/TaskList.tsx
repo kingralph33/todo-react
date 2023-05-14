35523ceace4e9b3476c7658fd7d079abdf53ed3b
Ralph King Jr <15459559+kingralph33@users.noreply.github.com>
2023-05-13 22:50:36 -0400

    formatted doc

--- src/Components/TaskList.tsx
+++ src/Components/TaskList.tsx
@@ -1,22 +1,25 @@
-export default function TaskList() { return ( <div className='border-4
-border-emerald-600 text-emerald-400 font-bold text-lg p-8'>
+export default function TaskList() {
+	return (
+		<div
+			className='border-4
+border-emerald-600 text-emerald-400 font-bold text-lg p-8'
+		>
 			<ul>
 				<li className='flex items-center border-b-2 border-emerald-600 mb-4'>
 					<label className='flex w-full mb-2'>
-						<p className="flex w-10/12">Another task</p>
+						<p className='flex w-10/12'>Another task</p>
 						<input
 							type='checkbox'
 							name='taskCompletedBox'
 							id='taskCompletedBox'
-                            className="flex w-5 mr-4 checked:accent-emerald-400"
+							className='flex w-5 mr-4 checked:accent-emerald-400'
 						/>
-                       <input
-						type='submit'
-						value='Delete'
-						className=' flex w-fit justify-self-end rounded-full bg-red-800 font-bold text-white text-md px-4 lg:px-8'
-					   />
+						<input
+							type='submit'
+							value='Delete'
+							className=' flex w-fit justify-self-end rounded-full bg-red-800 font-bold text-white text-md px-4 lg:px-8'
+						/>
 					</label>
-					
 				</li>
 			</ul>
 		</div>
